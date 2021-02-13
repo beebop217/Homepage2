971b2dda5b44f07145a85446a1d568dffa72d542
beebop217 <beeebop@protonmail.com>
2021-02-13 13:43:06 -0500

    Change to ruri

---
+++ themes/ruri-dark/mascots.js
@@ -0,0 +1,1 @@
+var mascotList = [ "ruri1.png", "ruri2.png", "ruri3.png", "ruri4.png", "ruri5.png", "ruri6.png", "ruri7.png", "ruri8.png", "ruri9.png", "ruri10.png", "ruri11.png", "ruri12.png", "ruri13.png", "ruri14.png", "ruri15.png" ];
